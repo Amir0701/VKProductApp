a05cc53bb928ceee39f11cce2cc98ffe7ba37df7
Amir0701 <amirusmonov2002@gmail.com>
2024-03-09 22:53:07 +0300

    ProductRecyclerAdapter was added

---
+++ app/src/main/java/com/example/vkproductapp/presentation/view/ProductRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.vkproductapp.presentation.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.vkproductapp.R
+import com.example.vkproductapp.data.model.Product
+
+class ProductRecyclerAdapter: RecyclerView.Adapter<ProductRecyclerAdapter.ProductViewHolder>() {
+    var products: List<Product> = emptyList()
+
+    class ProductViewHolder(private val itemView: View): RecyclerView.ViewHolder(itemView){
+        private val productImageView: ImageView = itemView.findViewById(R.id.cardProductImage)
+        private val productTitle: TextView = itemView.findViewById(R.id.cardProductTitle)
+        private val productDescription: TextView = itemView.findViewById(R.id.cardProductDescription)
+
+        fun bind(product: Product){
+            Glide.with(productImageView)
+                .load(product.thumbnail)
+                .into(productImageView)
+            productTitle.text = product.title
+            productDescription.text = product.description
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.product_recycler_item, parent, false)
+        return ProductViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val currentProduct = products[position]
+        holder.bind(currentProduct)
+    }
+}
