81229f8c809dea41c12cdd4b1419bf968a024c4e
Amir0701 <amirusmonov2002@gmail.com>
2024-03-09 22:53:57 +0300

    feature/get publications

--- app/src/main/java/com/example/vkproductapp/presentation/view/ProductsFragment.kt
+++ app/src/main/java/com/example/vkproductapp/presentation/view/ProductsFragment.kt
@@ -7,13 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.vkproductapp.R
 import com.example.vkproductapp.presentation.viewmodel.ProductsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class ProductsFragment : Fragment() {
-    val productsViewModel by viewModel<ProductsViewModel>()
+    private val productsViewModel by viewModel<ProductsViewModel>()
+    private var recyclerView: RecyclerView? = null
+    private val productRecyclerAdapter = ProductRecyclerAdapter()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,13 +29,25 @@ class ProductsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews(view)
+        setUpRecyclerView()
         observeOnProducts()
         productsViewModel.getProducts()
     }
 
+    private fun initViews(view: View){
+        recyclerView = view.findViewById(R.id.productsRecyclerView)
+    }
+
+    private fun setUpRecyclerView(){
+        recyclerView?.layoutManager = GridLayoutManager(context, 2)
+        recyclerView?.adapter = productRecyclerAdapter
+    }
     private fun observeOnProducts(){
         productsViewModel.productsLiveData.observe(viewLifecycleOwner) {responseData->
             Log.i("size",responseData.products.size.toString())
+            productRecyclerAdapter.products = responseData.products
+            productRecyclerAdapter.notifyDataSetChanged()
         }
     }
 }
