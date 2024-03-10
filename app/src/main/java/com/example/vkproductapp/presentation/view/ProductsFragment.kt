cfe72b365b399e50a92d6a7068ef5dd7e036752b
Amir0701 <amirusmonov2002@gmail.com>
2024-03-10 15:11:47 +0300

    feature/show dialog if request was failed

--- app/src/main/java/com/example/vkproductapp/presentation/view/ProductsFragment.kt
+++ app/src/main/java/com/example/vkproductapp/presentation/view/ProductsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.vkproductapp.presentation.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -77,6 +78,11 @@ class ProductsFragment : Fragment() {
 
                 is Result.Error ->{
                     progressBar?.visibility = View.GONE
+                    AlertDialog.Builder(requireContext())
+                        .setTitle("Error")
+                        .setMessage(responseResult.message)
+                        .create()
+                        .show()
                 }
             }
         }
