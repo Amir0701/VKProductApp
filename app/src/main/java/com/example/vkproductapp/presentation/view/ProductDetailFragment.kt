3735f44b8e87e04005d16e077de9fa30a14dbb1b
Amir0701 <amirusmonov2002@gmail.com>
2024-03-11 18:02:12 +0300

    feature/show detail information of product

--- app/src/main/java/com/example/vkproductapp/presentation/view/ProductDetailFragment.kt
+++ app/src/main/java/com/example/vkproductapp/presentation/view/ProductDetailFragment.kt
@@ -2,17 +2,25 @@ package com.example.vkproductapp.presentation.view
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavArgs
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.vkproductapp.R
 
 class ProductDetailFragment : Fragment() {
     private val navArgs by navArgs<ProductDetailFragmentArgs>()
-
+    private var imageSlider: ImageSlider? = null
+    private var productTitleTextView: TextView? = null
+    private var productPriceTextView: TextView? = null
+    private var productBrandTextView: TextView? = null
+    private var productCategoryTextView: TextView? = null
+    private var productDescriptionTextView: TextView? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,5 +32,37 @@ class ProductDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.i("TAG", navArgs.product.title)
+        initViews(view)
+        setImagesToSlider()
+        setProductData()
+    }
+
+    private fun initViews(view: View){
+        imageSlider = view.findViewById(R.id.productImageSlider)
+        productBrandTextView = view.findViewById(R.id.productBrand)
+        productCategoryTextView = view.findViewById(R.id.productCategory)
+        productPriceTextView = view.findViewById(R.id.productPrice)
+        productTitleTextView = view.findViewById(R.id.productTitle)
+        productDescriptionTextView = view.findViewById(R.id.description)
+    }
+
+    private fun setImagesToSlider(){
+        val slideModels = mapProductImageToSlideModel(navArgs.product.images)
+        imageSlider?.setImageList(slideModels, ScaleTypes.CENTER_CROP)
+    }
+
+    private fun mapProductImageToSlideModel(images: List<String>): List<SlideModel>{
+        return images.map {image->
+            SlideModel(image)
+        }
+    }
+
+    private fun setProductData(){
+        val product = navArgs.product
+        productTitleTextView?.text = product.title
+        productPriceTextView?.text = "${product.price} $"
+        productCategoryTextView?.text = product.category
+        productBrandTextView?.text = product.brand
+        productDescriptionTextView?.text = product.description
     }
 }
